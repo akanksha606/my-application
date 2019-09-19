6f42c4e6527b0a1584a0b2fa52217f816cff3fed
akanksha606 <akankshagupta98583@gmail.com>
2019-09-20 00:45:30 +0530

    Just Java

--- app/src/main/java/android/example/justjava/MainActivity.java
+++ app/src/main/java/android/example/justjava/MainActivity.java
@@ -15,7 +15,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.text.NumberFormat;
 
@@ -30,26 +33,53 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     int quantity=0;
+
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        String priceMessage="Total : $"+(quantity*5)+"\nThank You!!";
-        displayMessage(priceMessage);
+        CheckBox whippedCreamCheckbox=(CheckBox)findViewById(R.id.whipped_cream_checkbox);
+        CheckBox chocolateCheckbox=(CheckBox)findViewById(R.id.chocolate_checkbox);
+        EditText name=(EditText)findViewById(R.id.name_field);
+        String nameField=name.getText().toString();
+        boolean hasChocolate=chocolateCheckbox.isChecked();
+        boolean hasWhippedCream=whippedCreamCheckbox.isChecked();
+        displayMessage(createOrderSummary(nameField,5,hasWhippedCream,hasChocolate));
+
     }
+    public String createOrderSummary(String name,int price,boolean addWhippedCream,boolean addChocolate){
+        if(addWhippedCream)
+            price+=1;
+        if(addChocolate)
+            price+=2;
+        String summary="Name:"+name;
+        summary+="\nAdd Whipped Cream?"+addWhippedCream;
+        summary+="\nAdd Chocolate?"+addChocolate;
+        summary=summary+"\n Quantity:"+quantity+"\nTotal:$"+(quantity*price)+"\nThank You";
+        return summary;
+    }
+
     /**
      * This method displays the given text on the screen.
      */
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
     public void increment(View view) {
+        if(quantity>100) {
+            Toast.makeText(MainActivity.this,"You cannot have more than 100 coffees",Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity=quantity+1;
         display(quantity);
 
     }
     public void decrement(View view) {
+        if(quantity<1) {
+            Toast.makeText(MainActivity.this,"You cannot have less than 1 coffee",Toast.LENGTH_SHORT).show();
+            return;
+        }
         quantity=quantity-1;
         display(quantity);
 
@@ -67,7 +97,7 @@ public class MainActivity extends AppCompatActivity {
      * This method displays the given price on the screen.
      */
     private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
+        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
         priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
     }
 }
