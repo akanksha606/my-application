3e773fe49e8f52f49cdaeea6e45286333e7f108b
akanksha606 <akankshagupta98583@gmail.com>
2019-09-21 00:37:03 +0530

    Just Java

--- app/src/main/java/android/example/justjava/MainActivity.java
+++ app/src/main/java/android/example/justjava/MainActivity.java
@@ -11,6 +11,8 @@ package android.example.justjava;
 
 
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -32,55 +34,59 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-    int quantity=0;
+
+    int quantity = 0;
 
     /**
      * This method is called when the order button is clicked.
      */
     public void submitOrder(View view) {
-        CheckBox whippedCreamCheckbox=(CheckBox)findViewById(R.id.whipped_cream_checkbox);
-        CheckBox chocolateCheckbox=(CheckBox)findViewById(R.id.chocolate_checkbox);
-        EditText name=(EditText)findViewById(R.id.name_field);
-        String nameField=name.getText().toString();
-        boolean hasChocolate=chocolateCheckbox.isChecked();
-        boolean hasWhippedCream=whippedCreamCheckbox.isChecked();
-        displayMessage(createOrderSummary(nameField,5,hasWhippedCream,hasChocolate));
+        CheckBox whippedCreamCheckbox = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        CheckBox chocolateCheckbox = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        EditText name = (EditText) findViewById(R.id.name_field);
+        String nameField = name.getText().toString();
+        boolean hasChocolate = chocolateCheckbox.isChecked();
+        boolean hasWhippedCream = whippedCreamCheckbox.isChecked();
+        String message = createOrderSummary(nameField, 5, hasWhippedCream, hasChocolate);
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:")); // only email apps should handle this
+        intent.putExtra(Intent.EXTRA_SUBJECT, "Just java app for " + nameField);
+        intent.putExtra(Intent.EXTRA_TEXT, message);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
 
     }
-    public String createOrderSummary(String name,int price,boolean addWhippedCream,boolean addChocolate){
-        if(addWhippedCream)
-            price+=1;
-        if(addChocolate)
-            price+=2;
-        String summary="Name:"+name;
-        summary+="\nAdd Whipped Cream?"+addWhippedCream;
-        summary+="\nAdd Chocolate?"+addChocolate;
-        summary=summary+"\n Quantity:"+quantity+"\nTotal:$"+(quantity*price)+"\nThank You";
+
+    public String createOrderSummary(String name, int price, boolean addWhippedCream, boolean addChocolate) {
+        if (addWhippedCream)
+            price += 1;
+        if (addChocolate)
+            price += 2;
+        String summary = "Name:" + name;
+        summary += "\nAdd Whipped Cream?" + addWhippedCream;
+        summary += "\nAdd Chocolate?" + addChocolate;
+        summary = summary + "\n Quantity:" + quantity + "\nTotal:$" + (quantity * price) + "\nThank You";
         return summary;
     }
 
-    /**
-     * This method displays the given text on the screen.
-     */
-    private void displayMessage(String message) {
-        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        orderSummaryTextView.setText(message);
-    }
+
     public void increment(View view) {
-        if(quantity>100) {
-            Toast.makeText(MainActivity.this,"You cannot have more than 100 coffees",Toast.LENGTH_SHORT).show();
+        if (quantity > 100) {
+            Toast.makeText(MainActivity.this, "You cannot have more than 100 coffees", Toast.LENGTH_SHORT).show();
             return;
         }
-        quantity=quantity+1;
+        quantity = quantity + 1;
         display(quantity);
 
     }
+
     public void decrement(View view) {
-        if(quantity<1) {
-            Toast.makeText(MainActivity.this,"You cannot have less than 1 coffee",Toast.LENGTH_SHORT).show();
+        if (quantity < 1) {
+            Toast.makeText(MainActivity.this, "You cannot have less than 1 coffee", Toast.LENGTH_SHORT).show();
             return;
         }
-        quantity=quantity-1;
+        quantity = quantity - 1;
         display(quantity);
 
     }
@@ -93,11 +99,6 @@ public class MainActivity extends AppCompatActivity {
         TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
         quantityTextView.setText("" + number);
     }
-    /**
-     * This method displays the given price on the screen.
-     */
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.order_summary_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
-    }
+
 }
+
